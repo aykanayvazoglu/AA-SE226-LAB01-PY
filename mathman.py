bff77e4ce51f39b0d211a57207c155fa367c001e
aykanayvazoglu <127196379+aykanayvazoglu@users.noreply.github.com>
2023-03-07 16:02:42 +0300

    Add files via upload

---
+++ mathman.py
@@ -0,0 +1,6 @@
+var1 = int(input("Please enter a value: "))
+var2 = int(input("Please enter another: "))
+sum = var1 + var2
+diff = var1 - var2
+prod = var1 * var2
+print("The sum is ", sum, ", the difference is ", diff, ", and the product is ", prod, ".")
---
+++ myFirstLabScript.py
@@ -0,0 +1,4 @@
+x = input("Please enter your name: ")
+print("Hello " + x + "!")
+y = input("Please enter your ID: ")
+print("Understood. Your ID is " + y + ".")
